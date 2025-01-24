1fd41d971b2593eae7b31e67339939d771702a16
Florian Claux <159036680+fclaux@users.noreply.github.com>
2025-01-24 11:32:41 +0100

    TP1

---
+++ TP1/app/src/main/java/com/example/tp1/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.tp1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+const val EXTRA_TEXT = "text_to_display"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val rootView = findViewById<ConstraintLayout>(R.id.main)
+        rootView.setBackgroundResource(R.drawable.gradient_background)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val premierBouton : Button = findViewById(R.id.premierButton);
+        val textInput : EditText = findViewById(R.id.editTextText);
+        val textView : TextView = findViewById(R.id.textView);
+
+        val bouttonPageSuivante : Button = findViewById(R.id.buttonNextPage);
+
+        bouttonPageSuivante.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            intent.putExtra(EXTRA_TEXT, textView.text.toString())
+            startActivity(intent);
+        }
+
+
+        premierBouton.setOnClickListener {
+            if (textInput.text.toString() == "afficher nouveau textView") {
+                val layoutPrincipal : ConstraintLayout = findViewById(R.id.main);
+                val newTextView : TextView = TextView(this);
+                newTextView.text = textInput.text;
+                layoutPrincipal.addView(newTextView);
+            }
+
+
+            textView.text = textInput.text;
+        }
+    }
+}
---
+++ TP1/app/src/main/java/com/example/tp1/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.tp1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intent = intent
+        val title : TextView = findViewById<TextView>(R.id.titlePage2)
+        val titleText = intent.getStringExtra("text_to_display")?: "Pas de titre"
+
+        title.text = titleText
+
+
+    }
+}
