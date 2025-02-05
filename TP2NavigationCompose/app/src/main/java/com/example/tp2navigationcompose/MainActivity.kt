8f2a01554f4f048f2a765b5bec2826aca47671be
Florian Claux <159036680+fclaux@users.noreply.github.com>
2025-02-05 14:35:42 +0100

    fin TP2 Navigation Compose

--- TP2NavigationCompose/app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
+++ TP2NavigationCompose/app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
@@ -11,17 +11,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -65,11 +68,12 @@ fun AppNavigation() {
             FormScreen(navController = navController)
         }
         composable(
-            "showForm/{name}",
-            arguments = listOf(navArgument("name") {defaultValue =""}))
+            "showForm/{name}/{age}",
+            arguments = listOf(navArgument("name") {defaultValue =""}, navArgument("age") { defaultValue = 0 }))
         {   backStackEntry ->
             val name = backStackEntry.arguments?.getString("name") ?: ""
-            ShowFormScreen(navController = navController, name)
+            val age = backStackEntry.arguments?.getInt("age") ?: 0
+            ShowFormScreen(navController = navController, name, age)
         }
     }
 }
@@ -94,6 +98,7 @@ fun HomeScreen(navController : NavController) {
 @Composable
 fun FormScreen(navController : NavController) {
     var name by remember { mutableStateOf("")}
+    var age by remember { mutableIntStateOf(0) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -111,8 +116,18 @@ fun FormScreen(navController : NavController) {
                 .fillMaxWidth()
                 .padding(16.dp)
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = age.toString(),
+            onValueChange = { newText -> age = newText.toIntOrNull() ?: 0 },
+            label = { Text("Entrez votre âge") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
         Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = { navController.navigate("showForm/$name")}) {
+        Button(onClick = { navController.navigate("showForm/$name/$age")}) {
             Text(text = "Valider")
         }
         Spacer(modifier = Modifier.height(8.dp))
@@ -123,7 +138,7 @@ fun FormScreen(navController : NavController) {
 }
 
 @Composable
-fun ShowFormScreen(navController: NavController, name : String) {
+fun ShowFormScreen(navController: NavController, name : String, age : Int) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -134,6 +149,8 @@ fun ShowFormScreen(navController: NavController, name : String) {
         Text(text = "Affichage du formulaire", style = MaterialTheme.typography.titleMedium)
         Spacer(modifier = Modifier.height(24.dp))
         Text(text = name, style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Vous avez $age ans !", style = MaterialTheme.typography.titleMedium)
         Spacer(modifier = Modifier.height(24.dp))
         Button(onClick = { navController.popBackStack()}) {
             Text(text = "Retour")
